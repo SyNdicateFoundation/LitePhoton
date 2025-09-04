a376e3e6490dbec154a844140dd5029e7d0b5aca
realstresser <rezaebr3234s4@gmail.com>
2025-09-04 23:02:57 +0300

    Replace BufReader with MemMap2

--- src/read_util.rs
+++ src/read_util.rs
@@ -1,15 +1,12 @@
 use crate::input::Input;
 use crate::logger::log_error;
 use std::borrow::Cow;
-use std::io::{stdout, BufReader, BufWriter, Read, Seek, SeekFrom, Write};
+use std::io::{stdout, BufWriter, Write};
 use std::sync::Arc;
 use std::{cmp};
 use log::error;
+use memmap2::Mmap;
 use strum_macros::EnumString;
-/*
-TODO: Use MemMap2 instead of bufreader
-TODO: do memory calculations to avoid messing up system resources
- */
 
 /// Modes of reading
 /// Uses strum lib to convert Enums into Strings and parse them
@@ -30,52 +27,29 @@ pub fn read_input(mut mode: Mode, input: Input, keyword: &str) {
 
     match mode {
         Mode::Normal => {
+            let mmap = unsafe { Mmap::map(&input.open_file().unwrap()) }.expect("Failed to map file");
             let mut writer = BufWriter::new(stdout());
-            let mut reader = BufReader::with_capacity(8 * 1024, input.open_file().unwrap());
-            let mut read_buff = [0u8; 8 * 1024];
-            let mut line_buff = Vec::with_capacity(8 * 1024);
+            let mut begin = 0usize;
 
-            loop {
-                match reader.read(&mut read_buff) {
-                    Ok(0) => {
-                        writer.write_all(b"\n").expect("Can't write \\n");
-                        writer.flush().expect("failed to flush writer");
-                        break;
+            for (i, &byte) in mmap.iter().enumerate() {
+                if byte == b'\n' {
+                    let line = &mmap[begin..=i];
+                    if keyword.is_empty() || twoway::find_bytes(line, &keyword).is_some() {
+                        writer.write_all(line).expect("Can't write results");
                     }
-                    Ok(size) => {
-                        line_buff.extend_from_slice(&read_buff[..size]);
-
-                        let mut begin = 0usize;
-                        let mut i = 0usize;
-
-                        while i < line_buff.len() {
-                            if line_buff[i] == b'\n' {
-                                let line = &line_buff[begin..=i];
-
-                                if keyword.is_empty() || twoway::find_bytes(line, &keyword).is_some() {
-                                    writer.write_all(line).expect("Can't write results");
-                                }
-
-                                begin = i + 1;
-                            }
-                            i += 1;
-                        }
-
-                        if begin == 0 {
-                            continue;
-                        }
-
-                        if begin < line_buff.len() {
-                            line_buff.drain(0..begin);
-                        } else {
-                            line_buff.clear();
-                        }
-                    }
-                    Err(_) =>{
-                        writer.flush().expect("failed to flush writer");
-                    }
+                    begin = i + 1;
                 }
             }
+
+            if begin < mmap.len() {
+                let line = &mmap[begin..];
+                if keyword.is_empty() || twoway::find_bytes(line, &keyword).is_some() {
+                    writer.write_all(line).expect("Can't write results");
+                    writer.write_all(b"\n").expect("Can't write newline");
+                }
+            }
+
+            writer.flush().expect("failed to flush writer");
         }
         Mode::Chunk => {
             let input = match input {
@@ -86,6 +60,8 @@ pub fn read_input(mut mode: Mode, input: Input, keyword: &str) {
                 },
             };
             let file_size = input.metadata().unwrap().len();
+            let mmap = unsafe { Mmap::map(&input.open_file().unwrap()).expect("Failed to mmap file") };
+            let mmap = Arc::new(mmap);
             let num_workers = cmp::max(1, cmp::min(num_cpus::get(), 64)) as u64;
             let chunk_size = if file_size == 0 {0} else {(file_size + num_workers - 1) / num_workers};
             let keyword: Arc<Cow<[u8]>> = Arc::new(keyword);
@@ -93,7 +69,7 @@ pub fn read_input(mut mode: Mode, input: Input, keyword: &str) {
 
             for id in 0..num_workers {
                 let keyword = keyword.clone();
-                let input = input.clone();
+                let mmap = mmap.clone();
                 let begin = id * chunk_size;
 
                 if begin >= file_size {
@@ -104,62 +80,33 @@ pub fn read_input(mut mode: Mode, input: Input, keyword: &str) {
                     if id == num_workers - 1 { file_size } else { begin + chunk_size },
                     begin,
                 );
+
                 handlers.push(rayon::spawn(move || {
-                    let mut reader = BufReader::with_capacity(8 * 1024 * 1024, input.open_file().unwrap());
                     let mut writer = BufWriter::new(stdout().lock());
-                    let mut read_buff = [0u8; 8 * 1024];
                     let mut line_buff = Vec::with_capacity(8 * 1024);
-                    let mut pos = begin;
+                    let mut pos = begin as usize;
 
-                    reader.seek(SeekFrom::Start(begin)).unwrap();
+                    while pos < end as usize {
+                        let read_buff = mmap[pos];
 
-                    if begin > 0 {
-                        pos = pos.saturating_add(reader.read(&mut read_buff).unwrap() as u64);
-                    }
+                        line_buff.push(read_buff);
 
-                    while pos < end {
-                        match reader.read(&mut read_buff) {
-                            Ok(0) => {
-                                writer.write_all(b"\n").expect("Can't write \\n");
-                                writer.flush().expect("failed to flush writer");
-                                break;
-                            },
-                            Ok(size) => {
-                                pos = pos.saturating_add(size as u64);
-
-                                line_buff.extend_from_slice(&read_buff[..size]);
-
-                                let mut begin = 0usize;
-                                let mut i = 0usize;
-
-                                while i < line_buff.len() {
-                                    if line_buff[i] == b'\n' {
-                                        let line = &line_buff[begin..=i];
-
-                                        if keyword.is_empty() || twoway::find_bytes(line, &keyword).is_some() {
-                                            writer.write_all(line).expect("Can't write results");
-                                        }
-
-                                        begin = i + 1;
-                                    }
-                                    i += 1;
-                                }
-
-                                if begin == 0 {
-                                    continue;
-                                }
-
-                                if begin < line_buff.len() {
-                                    line_buff.drain(0..begin);
-                                } else {
-                                    line_buff.clear();
-                                }
+                        if read_buff == b'\n' {
+                            if keyword.is_empty() || twoway::find_bytes(&line_buff, &keyword).is_some() {
+                                writer.write_all(&line_buff).expect("Can't write results");
                             }
-                            Err(_) => {
-                                writer.flush().expect("failed to flush writer");
-                            },
-                        };
+
+                            line_buff.clear();
+                        }
+
+                        pos += 1;
                     }
+
+                    if !line_buff.is_empty() && (keyword.is_empty() || twoway::find_bytes(&line_buff, &keyword).is_some()) {
+                        writer.write_all(&line_buff).expect("Can't write results");
+                    }
+
+                    writer.flush().expect("failed to flush writer");
                 }));
             }
 
